fd3d7a184ef72470e854747a2d39b7b022d2ea17
Fedor <fedor531@yandex.ru>
2021-10-20 14:54:47 +0200

    feat

--- keys/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URL: `mongodb+srv://fedor:o6FxUX8BEUL4rfYK421fsasSag2@cluster0.akvwt.mongodb.net/shop`,
-    SESSION_SECRET: 'some secret value',
-    SEND_GRID_API_KEY: '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',
-    EMAIL_FROM: 'awesome.fedr541@yandex.ru',
-    BASE_URL: 'http://localhost:3000'
-}
